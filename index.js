4590d290e0849341784df23697a6e354cc52f9cf
Oliver de Sousa <oliver.seq@gmail.com>
2018-08-26 20:24:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(katzDeli){
   var line = [];
   if (katzDeli.length === 0) return "The line is currently empty.";
   for (var i = 0; i < katzDeli.length; i++){
-    line.push( i+1 + ". " + katzDeli[i] + ", ");
+    line.push( i+1 + ". " + katzDeli[i]);
   }
   return "The line is currently: " + line;
 }
