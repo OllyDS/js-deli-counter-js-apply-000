6a11509813b20a87a4f4237e3391c974f5422dce
Oliver de Sousa <oliver.seq@gmail.com>
2018-08-26 20:03:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var katzDeli = [];
 var otherDeli = ["Steven", "Blake", "Avi"];
 
 function takeANumber(katzDeli, name){
-  return katzDeli.push("Welcome, " +name+ ". You are number " +katzDeli.length+ " in line.");
+  katzDeli.push("Welcome, " +name+ ". You are number " +katzDeli.length+ " in line.");
 }
+return katzDeli;
